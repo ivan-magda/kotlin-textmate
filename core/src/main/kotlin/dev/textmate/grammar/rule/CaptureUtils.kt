0bd9f9cc1670796850585a3a039f5e790dcc54bb
Ivan Magda <imagda15@gmail.com>
2026-02-23 20:19:05 +0300

    Refactor CaptureUtils to use constant for transform group index

--- core/src/main/kotlin/dev/textmate/grammar/rule/CaptureUtils.kt
+++ core/src/main/kotlin/dev/textmate/grammar/rule/CaptureUtils.kt
@@ -3,6 +3,7 @@ package dev.textmate.grammar.rule
 import dev.textmate.regex.CaptureIndex
 
 private val CAPTURING_REGEX_SOURCE = Regex("""\$(\d+)|\$\{(\d+):/(downcase|upcase)\}""")
+private const val TRANSFORM_GROUP_INDEX = 3
 
 internal fun hasCaptures(regexSource: String?): Boolean =
     if (regexSource == null) {
@@ -19,13 +20,14 @@ internal fun replaceCaptures(
     CAPTURING_REGEX_SOURCE.replace(regexSource) { matchResult ->
         val index = (matchResult.groupValues[1].ifEmpty { matchResult.groupValues[2] }).toInt()
         val capture = captureIndices.getOrNull(index)
+
         if (capture != null) {
             var result = captureSource.substring(capture.start, capture.end)
             // Remove leading dots that would make the selector invalid
             while (result.startsWith('.')) {
                 result = result.substring(1)
             }
-            when (matchResult.groupValues[3]) {
+            when (matchResult.groupValues[TRANSFORM_GROUP_INDEX]) {
                 "downcase" -> result.lowercase()
                 "upcase" -> result.uppercase()
                 else -> result
