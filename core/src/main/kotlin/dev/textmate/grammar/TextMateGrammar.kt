eb0edce12f23e1cf7e9851b093b146b25aa8cd37
Ivan Magda <imagda15@gmail.com>
2026-02-23 20:46:39 +0300

    Refactor `TextMateGrammar` to convert class into an object and simplify version declaration

--- core/src/main/kotlin/dev/textmate/grammar/TextMateGrammar.kt
+++ core/src/main/kotlin/dev/textmate/grammar/TextMateGrammar.kt
@@ -1,7 +1,5 @@
 package dev.textmate.grammar
 
-class TextMateGrammar {
-    companion object {
-        const val VERSION = "0.1.0-SNAPSHOT"
-    }
+object TextMateGrammar {
+    const val VERSION = "0.1.0-SNAPSHOT"
 }
